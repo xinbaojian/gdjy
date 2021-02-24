2a4e9ea2224d0963b168ed079b56ddbc17a4b493
baojian <baojian.xin.py@gmail.com>
2021-02-24 16:57:29 +0800

    模板修改

--- js/PageAll.js
+++ js/PageAll.js
@@ -14,12 +14,3 @@ $(document).ready(function(){
 	};  
 
 });
-
-$(function(){
-//百度分享
-window._bd_share_config={
-	"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"2","bdSize":"24"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
-//百度分享结束
-
-	
-});
